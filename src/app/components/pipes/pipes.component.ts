a3fe698e86bf91b07e30531bf51702972bc3e182
MiguDeveloper <miguel.peru.seo@gmail.com>
2021-01-20 17:28:13 -0500

    improve variables

--- src/app/components/pipes/pipes.component.ts
+++ src/app/components/pipes/pipes.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-pipes",
-  templateUrl: "./pipes.component.html",
-  styleUrls: ["./pipes.component.css"],
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css'],
 })
 export class PipesComponent implements OnInit {
-  nombre = "bill gates";
-  nombre2 = "steve wozniak";
+  nombre = 'bill gates';
+  nombre2 = 'steve wozniak';
   numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  personajes = ["Ironman", "Spiderman", "Thor", "Loki", "Groot"];
+  personajes = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
   PI = Math.PI;
   porcentaje = 0.234;
-  salario: number = 12345;
+  salario = 12345;
   heroe = {
-    nombre: "Logan",
-    clave: "Wolverine",
+    nombre: 'Logan',
+    clave: 'Wolverine',
     edad: 500,
     direccion: {
-      calle: "Primera",
+      calle: 'Primera',
       casa: 20,
     },
   };
   fecha = new Date();
   activar = true;
-  constrasenia = "estaeslacontrasenia";
+  constrasenia = 'estaeslacontrasenia';
 
   valorPromesa = new Promise<string>((resolve) => {
     setTimeout(() => {
-      resolve("Llego la data");
+      resolve('Llego la data');
     }, 4500);
   });
 
--- src/app/pipes/contrasenia.pipe.ts
+++ src/app/pipes/contrasenia.pipe.ts
@@ -1,10 +1,10 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: "contrasenia",
+  name: 'contrasenia',
 })
 export class ContraseniaPipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): string {
-    return args[0] ? "*".repeat(value.length) : value;
+    return args[0] ? '*'.repeat(value.length) : value;
   }
 }
